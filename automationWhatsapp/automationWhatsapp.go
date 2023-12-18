a13dd900070c037eeb9969e5d75a03aa2d1cbd9e
dsonbaker <araujoedson790@gmail.com>
2023-12-18 12:52:36 -0300

    update

--- automationWhatsapp/automationWhatsapp.go
+++ automationWhatsapp/automationWhatsapp.go
@@ -88,9 +88,8 @@ func Run() {
 		if IsOnWhatsAppResponse[0].IsIn {
 			GetProfilePictureInfoResponse, errGetProfile := client.GetProfilePictureInfo(IsOnWhatsAppResponse[0].JID, nil)
 			if errGetProfile != nil {
-				if errGetProfile.Error() != "the user has hidden their profile picture from you" {
-					panic(errGetProfile)
-				}
+				fmt.Println("error:", errGetProfile.Error())
+				panic(errGetProfile)
 			}
 			WriteToFile("all-numbers.txt", numberphone+"\n", "./numberphone/")
 			if GetProfilePictureInfoResponse.URL != "" {
