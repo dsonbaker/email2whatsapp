1b4a50dbefd852df25862dd0d84e53066ab74304
dsonbaker <araujoedson790@gmail.com>
2023-12-18 12:47:40 -0300

    error user "the user has hidden their profile picture from you"

--- automationWhatsapp/automationWhatsapp.go
+++ automationWhatsapp/automationWhatsapp.go
@@ -87,7 +87,7 @@ func Run() {
 		}
 		if IsOnWhatsAppResponse[0].IsIn {
 			GetProfilePictureInfoResponse, errGetProfile := client.GetProfilePictureInfo(IsOnWhatsAppResponse[0].JID, nil)
-			if errGetProfile != nil {
+			if errGetProfile.Error() != "the user has hidden their profile picture from you" && errGetProfile != nil {
 				panic(errGetProfile)
 			}
 			WriteToFile("all-numbers.txt", numberphone+"\n", "./numberphone/")
